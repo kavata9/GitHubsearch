356cfd97a31251ca8dbdeadb4c561abdbfb989b3
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-04-28 16:03:27 +0300

    correct the error to be able to find repos

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,33 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ProfileService} from '../profile.service';
+import { ProfileService } from '../profile.service';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-   profile:any[];
-   repose:any[];
-   username:string;
+profile: any[];
+repos: any[];
+username: string;
 
-  constructor(private profileService :ProfileService) {
-   
+  constructor(private profileService: ProfileService) {
    }
-   findProfile(){
+
+   findProfile() {
     this.profileService.updateProfile(this.username);
     this.profileService.getProfileInfo().subscribe(profile => {
-      // console.log(profile)
-      this.profile=profile;
-          })
-          this.profileService.getProfileRepos().subscribe(repos => {
-            // console.log(repos);
-            this.repose = repos;
-          });
+      console.log(profile);
+      this.profile = profile;
+    });
 
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
    }
-  
-
-
   ngOnInit() {
   }
 
